b062e88dea6b9278ee754885a4509e93b5c64f37
Zotoavina <zotoavinanantenaina@gmail.com>
2021-03-30 08:11:47 +0200

    sf

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/modele-angular'));
+app.use(express.static(__dirname + '/dist/ModeleAngular'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/modele-angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ModeleAngular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
